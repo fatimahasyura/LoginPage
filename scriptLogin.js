30703f35ce8477b6839d9e17d8ddc1d257e9e172
fatimahasyura <114643989+fatimahasyura@users.noreply.github.com>
2022-10-25 15:32:48 +0800

    text

---
+++ scriptLogin.js
@@ -0,0 +1,25 @@
+function login(){
+    let pass = document.getElementById('pass').value;
+    let user = document.getElementById('user').value;
+    let UppercaseReq = /(?=.*[A-Z])/;
+    let LowercaseReq = /(?=.*[a-z])/;
+    let NumberReq = /(?=.*[0-9])/;
+    let SymbolReq = /(?=.*[#,_^!@*&])/;
+    if (pass.length < 12) {
+        alert('Sorry, your password less than 12. Please insert again');
+    } else if (user == '') {
+		alert("Please enter a username.");
+	} else if (user.length < 6) {
+		alert("Username atleast 6 or more character");
+	} else if (!UppercaseReq.test(pass)) {
+        alert("The password need to include atleast ONE uppercase.");
+    } else if (!LowercaseReq.test(pass)) {
+        alert("The password need to include atleast ONE lowercase.");
+    } else if (!NumberReq.test(pass)) {
+        alert("The password need to include atleast ONE number.");
+    } else if (!SymbolReq.test(pass)) {
+        alert("The password need to include atleast ONE symbol.");
+    } else {
+        alert('Login Succesfully!');
+    }
+}
